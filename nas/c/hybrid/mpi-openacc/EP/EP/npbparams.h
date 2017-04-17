322ba4f795687a53c32cc01d42a3f149338c49ae
Aamir Majeed <aamirmaj@nightingale.udel.edu>
2017-04-17 19:46:14 -0400

    adding data

---
+++ nas/c/hybrid/mpi-openacc/EP/EP/npbparams.h
@@ -0,0 +1,19 @@
+/* CLASS = C */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define CLASS  'C'
+#define M      32
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "17 Apr 2017"
+#define NPBVERSION "3.3.1"
+#define CS1 "mpicc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-acc -ta=tesla:cc60,cc35 -Minfo=accel"
+#define CS6 "-acc -ta=tesla:cc60,cc35 -Minfo=accel"
+#define CS7 "randdp"
