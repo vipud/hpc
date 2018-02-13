28d3212a3743d7b42fc6a891f756e383c616d250
efwright <efwright@udel.edu>
2018-02-13 10:27:30 -0500

    more debug

--- f17/namd/ppm_one/common/common/traj.cpp
+++ f17/namd/ppm_one/common/common/traj.cpp
@@ -1455,6 +1455,12 @@ void CTraj::getani(ani_group *index, int index_size, proton *select, int select_
 			i1=index[j].pos[0]+base-1;
 			i2=index[j].pos[1]+base-1;
 			i3=index[j].pos[2]+base-1;
+			if(j==0)
+			{
+				debug_d[0] = index[0].pos[0];
+				debug_d[1] = index[0].pos[1];
+				debug_d[2] = index[0].pos[2];
+			}
 			center[0]=(my_x_arr[i1]+my_x_arr[i2]+my_x_arr[i3])/3;
 			center[1]=(my_y_arr[i1]+my_y_arr[i2]+my_y_arr[i3])/3; //x,y, and z are still vectors! look at Ctraj::loadcoor , this is where they get allocated
 			center[2]=(my_z_arr[i1]+my_z_arr[i2]+my_z_arr[i3])/3; //x,y, and z ->> change to x_arr , x_size .....
@@ -1490,9 +1496,9 @@ void CTraj::getani(ani_group *index, int index_size, proton *select, int select_
 					debug_i[0] = i1;
 					debug_i[1] = i2;
 					debug_i[2] = i3;
-					debug_d[0] = ori[0];
-					debug_d[1] = ori[1];
-					debug_d[2] = ori[2];
+					//debug_d[0] = ori[0];
+					//debug_d[1] = ori[1];
+					//debug_d[2] = ori[2];
 					debug_d[3] = v1[0];
 					debug_d[4] = v1[1];
 					debug_d[5] = v1[2];
