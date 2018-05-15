64be73f25da4094e7f099d8e946c81ac51bc5c57
jhdavis8 <jhdavis@udel.edu>
2018-05-15 13:05:35 -0400

    Adding the book's code for lists, started finelist

---
+++ civl/sets/ShavitCh9/Lists/test/lists/CoarseListTest.java
@@ -0,0 +1,151 @@
+/*
+ * FineHashSetTest.java
+ * JUnit based test
+ *
+ * Created on December 30, 2005, 12:14 AM
+ */
+package lists;
+
+import junit.framework.*;
+
+/**
+ * @author Maurice Herlihy
+ */
+public class CoarseListTest extends TestCase {
+  
+  private final static int THREADS = 8;
+  private final static int TEST_SIZE = 128;
+  private final static int PER_THREAD = TEST_SIZE / THREADS;
+  CoarseList<Integer> instance;
+  Thread[] thread = new Thread[THREADS];
+  
+  public CoarseListTest(String testName) {
+    super(testName);
+    instance = new CoarseList<Integer>();
+  }
+  
+  public static Test suite() {
+    TestSuite suite = new TestSuite(CoarseListTest.class);
+    
+    return suite;
+  }
+  
+  /**
+   * Sequential calls.
+   */
+  public void testSequential() {
+    System.out.println("sequential add, contains, and remove");
+    
+    for (int i = 0; i < TEST_SIZE; i++) {
+      instance.add(i);
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+      if (!instance.contains(i)) {
+        fail("bad contains: " + i );
+      }
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+      if (!instance.remove(i)) {
+        fail("bad remove: " + i );
+      }
+    }
+  }
+  /**
+   * Parallel add, sequential removes
+   * @throws java.lang.Exception 
+   */
+  public void testParallelAdd()  throws Exception {
+    System.out.println("parallel add");
+    for (int i = 0; i < THREADS; i++) {
+      thread[i] = new AddThread(i * PER_THREAD);
+    }
+    for (int i = 0; i < THREADS; i ++) {
+      thread[i].start();
+    }
+    for (int i = 0; i < THREADS; i ++) {
+      thread[i].join();
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+      if (!instance.contains(i)) {
+        fail("bad contains: " + i );
+      }
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+      if (!instance.remove(i)) {
+        fail("bad remove: " + i );
+      }
+    }
+  }
+
+  /**
+   * Sequential adds, parallel removes
+   * @throws java.lang.Exception 
+   */
+  public void testParallelRemove()  throws Exception {
+    System.out.println("parallel remove");
+    for (int i = 0; i < TEST_SIZE; i++) {
+      instance.add(i);
+    }
+    for (int i = 0; i < TEST_SIZE; i++) {
+      if (!instance.contains(i)) {
+        fail("bad contains: " + i );
+      }
+    }
+    for (int i = 0; i < THREADS; i++) {
+      thread[i] = new RemoveThread(i * PER_THREAD);
+    }
+    for (int i = 0; i < THREADS; i ++) {
+      thread[i].start();
+    }
+    for (int i = 0; i < THREADS; i ++) {
+      thread[i].join();
+    }
+  }
+  
+  /**
+   * Parallel adds, removes
+   * @throws java.lang.Exception 
+   */
+  public void testParallelBoth()  throws Exception {
+    System.out.println("parallel both");
+    Thread[] myThreads = new Thread[2 * THREADS];
+    for (int i = 0; i < THREADS; i++) {
+      myThreads[i] = new AddThread(i * PER_THREAD);
+      myThreads[i + THREADS] = new RemoveThread(i * PER_THREAD);
+    }
+    for (int i = 0; i < 2 * THREADS; i ++) {
+      myThreads[i].start();
+    }
+    for (int i = 0; i < 2 * THREADS; i ++) {
+      myThreads[i].join();
+    }
+  }
+  class AddThread extends Thread {
+    int value;
+    AddThread(int i) {
+      value = i;
+    }
+    public void run() {
+      for (int i = 0; i < PER_THREAD; i++) {
+        if (value + i == 128) {
+          int z =0;
+        }
+        instance.add(value + i);
+      }
+    }
+  }
+  class RemoveThread extends Thread {
+    int value;
+    RemoveThread(int i) {
+      value = i;
+    }
+    public void run() {
+      for (int i = 0; i < PER_THREAD; i++) {
+        if (!instance.remove(value + i)) {
+          fail("RemoveThread: duplicate remove: " + (value + i));
+        }
+      }
+    }
+  }
+  
+}
