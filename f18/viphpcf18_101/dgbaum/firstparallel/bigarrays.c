c3254d7e9b77e93f52dff771a0ad6beafb6925dc
Derek Baum <dgbaum@yoda.crpl.cis.udel.edu>
2018-10-03 19:22:49 -0400

    Made it work

--- f18/viphpcf18_101/dgbaum/firstparallel/bigarrays.c
+++ f18/viphpcf18_101/dgbaum/firstparallel/bigarrays.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <stdbool.h>
 
 int main(){
   int size = 10000; //this is the largest I can make the arrays...
@@ -26,7 +27,9 @@ int main(){
   for(int i = 0; i < size; i++)
     sumArray[i] = (int *)malloc(size * sizeof(int));
   
-  
+  int **sumArray2 = (int **)malloc(size * sizeof(int *));
+  for(int i = 0; i < size; i++)
+    sumArray2[i] = (int *)malloc(size * sizeof(int));
   
 
   srand((int) time(NULL));
@@ -41,37 +44,54 @@ int main(){
   }
 
   //here we will do the normal adding
-  clock_t start,end;
-  double cpu_time_used;
+  double start,end;
+  double time_elapsed;
 
-   start = clock();
+  start = omp_get_wtime();
 
   for(int i = 0; i < size; i++){
     for(int j = 0; j < size; j++){
-      sumArray[i][j] = array1[i][j] * array2[i][j];
+      sumArray[i][j] = array1[i][j] + array2[i][j];
     }
   }
 
-   end = clock();
+  end = omp_get_wtime();
 
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  time_elapsed = ((double) (end - start));
 
-  printf("Normal adding took %f seconds.\n",cpu_time_used);
+  printf("Normal adding took %f seconds.\n",time_elapsed);
   
-  start = clock();
+  start = omp_get_wtime();
   //here we will do the parallel adding
   int i,j;
-#pragma omp parallel for default(none) shared(sumArray,size,array1,array2) private(i,j)
+#pragma omp parallel for default(none) shared(sumArray2,size,array1,array2) private(i,j) collapse(2)
   for(i = 0; i < size; i++){
     for(j = 0; j < size; j++){
-      sumArray[i][j] = array1[i][j] * array2[i][j];
+      sumArray2[i][j] = array1[i][j] + array2[i][j];
     }
   }
 
-  end = clock();
+  end = omp_get_wtime();
 
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  time_elapsed = ((double) (end - start));
 
-  printf("Adding with parallelism took %f seconds.\n",cpu_time_used);
+  printf("Adding with parallelism took %f seconds.\n",time_elapsed);
+
+  //verifying that the parallel adding is adding properly.
+  bool same = true;
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
+      if(sumArray[i][j] != sumArray2[i][j]){
+	same = false;
+	i = j = size;
+	break;
+      }
+    }
+  }
+  if(same){
+    printf("Our parallel code is producing the proper solution.\n");
+  }else{
+    printf("Our parallel code does something wrong and doesn't work properly.\n");
+  }
   return 0;
 }
