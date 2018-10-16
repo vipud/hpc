b9505a85373455f27268f8008481479f15b7938a
Derek Baum <dgbaum@yoda.crpl.cis.udel.edu>
2018-10-16 17:53:04 -0400

    updating

--- f18/viphpcf18_101/dgbaum/nas_code/SNU_NPB-1.0.3/NPB3.3-SER-C/EP/npbparams.h
+++ f18/viphpcf18_101/dgbaum/nas_code/SNU_NPB-1.0.3/NPB3.3-SER-C/EP/npbparams.h
@@ -1,14 +1,14 @@
-/* CLASS = W */
+/* CLASS = E */
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
    in this directory. Do not modify it by hand.   
 */
-#define CLASS  'W'
-#define M      25
+#define CLASS  'E'
+#define M      40
 
 #define CONVERTDOUBLE  false
-#define COMPILETIME "09 Oct 2018"
+#define COMPILETIME "16 Oct 2018"
 #define NPBVERSION "3.3.1"
 #define CS1 "gcc"
 #define CS2 "$(CC)"
