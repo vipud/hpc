cfcfc9148db1e2541afb11c85e03ef5d1202e42a
Derek Baum <dgbaum@yoda.crpl.cis.udel.edu>
2018-11-26 16:45:23 -0500

    first gprof data on class w

--- f18/viphpcf18_101/dgbaum/nas_code/SNU_NPB-1.0.3/NPB3.3-OMP-C/EP/npbparams.h
+++ f18/viphpcf18_101/dgbaum/nas_code/SNU_NPB-1.0.3/NPB3.3-OMP-C/EP/npbparams.h
@@ -1,14 +1,14 @@
-/* CLASS = S */
+/* CLASS = W */
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
    in this directory. Do not modify it by hand.   
 */
-#define CLASS  'S'
-#define M      24
+#define CLASS  'W'
+#define M      25
 
 #define CONVERTDOUBLE  false
-#define COMPILETIME "16 Oct 2018"
+#define COMPILETIME "26 Nov 2018"
 #define NPBVERSION "3.3.1"
 #define CS1 "gcc"
 #define CS2 "$(CC)"
